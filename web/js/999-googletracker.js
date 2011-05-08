6e7b77f93783e61a894a3471874c16ecc4708a05
Jason A. Donenfeld <Jason@zx2c4.com>
2011-05-07 23:25:27 -0400

    Track google analytics.

---
+++ web/js/999-googletracker.js
@@ -0,0 +1,15 @@
+$(window).load(function () {
+	window._gaq = window._gaq || [];
+	window._gaq.push(['_setAccount', 'UA-135234-2']);
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+});
+$(window).hashchange(function() {
+	window._gaq = window._gaq || [];
+	window._gaq.push(['_trackPageview']);
+	window._gaq.push(['_trackPageview', location.hash.substring(1)]);
+});
