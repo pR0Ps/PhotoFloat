86e9456e57d509018069e0e7e571fa8841522faf
Jason A. Donenfeld <Jason@zx2c4.com>
2016-02-29 16:27:18 +0100

    Use HTTPS

--- deployment-config.mk
+++ deployment-config.mk
@@ -1,5 +1,5 @@
 WEB_SERVER := metheny.zx2c4.com
-WEB_SERVER_URL := http://photos.jasondonenfeld.com
+WEB_SERVER_URL := https://photos.jasondonenfeld.com
 
 HTDOCS_PATH := /var/www/htdocs/photos.jasondonenfeld.com
 HTDOCS_USER := nginx
