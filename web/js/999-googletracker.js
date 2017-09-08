3e82c71c643e398e28648b5e0878369cb944dec1
Graham McGregor <graham@gmcgregor.ca>
2017-09-07 22:14:34 -0400

    Make code prettier
    Recreatable with:
    
        cd web
        npx prettier --write {js,css}/*

--- web/js/999-googletracker.js
+++ web/js/999-googletracker.js
@@ -1,15 +1,15 @@
-$(window).on('load', function () {
-	window._gaq = window._gaq || [];
-	window._gaq.push(['_setAccount', 'UA-XXXXXX-XXX']);
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = 'https://www.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
+$(window).on("load", function() {
+  window._gaq = window._gaq || [];
+  window._gaq.push(["_setAccount", "UA-XXXXXX-XXX"]);
+  var ga = document.createElement("script");
+  ga.type = "text/javascript";
+  ga.async = true;
+  ga.src = "https://www.google-analytics.com/ga.js";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(ga, s);
 });
-$(window).on('hashchange', function() {
-	window._gaq = window._gaq || [];
-	window._gaq.push(['_trackPageview']);
-	window._gaq.push(['_trackPageview', PhotoFloat.cleanHash(location.hash)]);
+$(window).on("hashchange", function() {
+  window._gaq = window._gaq || [];
+  window._gaq.push(["_trackPageview"]);
+  window._gaq.push(["_trackPageview", PhotoFloat.cleanHash(location.hash)]);
 });
